4a38d20a3b5a32819c9dc583982ac7a0b53d9e5e
Destructionuser82 <adumarathe@gmail.com>
2015-11-24 18:40:32 -0800

    revised servoFINAL

---
+++ ControlMode.java
@@ -0,0 +1,193 @@
+/* Copyright (c) 2014 Qualcomm Technologies Inc
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of Qualcomm Technologies Inc nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.LightSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.hardware.UltrasonicSensor;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * TeleOp Mode
+ * <p>
+ * Enables control of the robot via the gamepad
+ */
+public class K9TeleOp extends OpMode {
+
+	/*
+	 * Note: the configuration of the servos is such that
+	 * as the arm servo approaches 0, the arm position moves up (away from the floor).
+	 * Also, as the claw servo approaches 0, the claw opens up (drops the game element).
+	 */
+
+	DcMotor motorRight;
+	DcMotor motorLeft;
+
+	Servo servo;
+
+	double servoIncrement = 0.1;
+
+	//Scale constant to scale the joystick value by
+	double SCALE = 0.01;
+	//Variable for the servo's current position
+	double currentPosition;
+
+
+	public K9TeleOp() {
+
+	}
+
+	/*
+	 * Code to run when the op mode is initialized goes here
+	 *
+	 * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#init()
+	 */
+	@Override
+	public void init() {
+
+
+		/*
+		 * Use the hardwareMap to get the dc motors and servos by name. Note
+		 * that the names of the devices must match the names used when you
+		 * configured your robot and created the configuration file.
+		 */
+
+		// Initialize motors
+		motorRight = hardwareMap.dcMotor.get("motor_2");
+		motorLeft = hardwareMap.dcMotor.get("motor_1");
+		motorLeft.setDirection(DcMotor.Direction.REVERSE);
+
+		// Initialize servos
+		servo = hardwareMap.servo.get("servo_1");
+		//Set the current position to a known value
+		currentPosition = servo.getPosition();
+	}
+
+	/*
+	 * This method will be called repeatedly in a loop
+	 *
+	 * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#run()
+	 */
+	@Override
+	public void loop() {
+
+		/*
+		 * Left joystick: Robot Drive
+		 * Y: open gate
+		 * A: close gate
+		 */
+
+		float throttle = -gamepad1.left_stick_y;
+		float direction = -gamepad1.left_stick_x;
+		float right = throttle - direction;
+		float left = throttle + direction;
+
+		// clip the right/left values so that the values never exceed +/- 1
+		right = Range.clip(right, -1, 1);
+		left = Range.clip(left, -1, 1);
+
+		/* Scale the joystick value to make it easier to control
+		 * the robot more precisely at slower speeds. */
+		right = (float)scaleInput(right);
+		left =  (float)scaleInput(left);
+
+		// write the values to the motors
+		motorRight.setPower(right);
+		motorLeft.setPower(left);
+
+		float servoDirection = -gamepad1.right_stick_x;
+		servoDirection = Range.clip(servoDirection, 0, 1);
+		
+		/*Open gates when 'y' is pressed
+		if(gamepad1.y) {
+			// RangeClip
+
+			// Increment each servo so that the gates open
+			currentPosition += servoIncrement;
+
+			// Write the new positions to the servos
+			servo.setPosition(currentPosition);
+		}
+		//Close gates when 'a' is pressed
+		if(gamepad1.a) {
+			// Increment each servo so that the gates close
+			currentPosition = currentPosition - 0.1;
+
+			// Write the new positions to the servos
+			servo.setPosition(currentPosition);
+		}*/
+
+
+	}
+
+	/*
+	 * Code to run when the op mode is first disabled goes here
+	 *
+	 * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#stop()
+	 */
+	@Override
+	public void stop() {
+
+	}
+
+	/*
+	 * This method scales the joystick input so for low joystick values, the
+	 * scaled value is less than linear.  This is to make it easier to drive
+	 * the robot more precisely at slower speeds.
+	 */
+	double scaleInput(double dVal)  {
+		double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+				0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
+
+		// get the corresponding index for the scaleInput array.
+		int index = (int) (dVal * 16.0);
+		if (index < 0) {
+			index = -index;
+		} else if (index > 16) {
+			index = 16;
+		}
+
+		double dScale = 0.0;
+		if (dVal < 0) {
+			dScale = -scaleArray[index];
+		} else {
+			dScale = scaleArray[index];
+		}
+
+		return dScale;
+	}
+
+}
